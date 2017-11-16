99d77fc3b191f5454eceed5b8833a3ab5c589a07
Johan Olofsson <j.olofsson@mail.com>
2017-11-16 10:44:05 +0100

    DevController, DevViewModel

---
+++ CaseRod/CaseRod/Controllers/DevController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CaseRod.Models;
+using CaseRod.ViewModels;
+
+namespace CaseRod.Controllers
+{
+    public class DevController : Controller
+    {
+        private ApplicationDbContext _database = new ApplicationDbContext();
+
+        public ActionResult Index()
+        {
+            var Model = new DevViewModel
+            {
+                Blades = _database.Blades.ToList(),
+                Handles = _database.Handles.ToList(),
+                ReelSeats = _database.ReelSeats.ToList()
+            };
+
+            return View(Model);
+        }
+    }
+}
---
+++ CaseRod/CaseRod/ViewModels/DevViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CaseRod.Models;
+
+namespace CaseRod.ViewModels
+{
+    public class DevViewModel
+    {
+        public List<Blade> Blades { get; set; }
+        public List<Handle> Handles { get; set; }
+        public List<ReelSeat> ReelSeats { get; set; }
+    }
+}
