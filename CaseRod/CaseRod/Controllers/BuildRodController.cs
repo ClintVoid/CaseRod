9874981ed065e99db95b8a2daf523067a4ee59f2
Parnia Parsa <parniaparsa@icloud.com>
2017-11-16 11:02:24 +0100

    buildrodcontroller

---
+++ CaseRod/CaseRod/Controllers/BuildRodController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaseRod.Controllers
+{
+    public class BuildRodController : Controller
+    {
+        // GET: BuildRod
+        public ActionResult BuildRod()
+        {
+            return View();
+        }
+    }
+}
