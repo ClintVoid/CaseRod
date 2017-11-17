649a65863ab384d90a0a8515be81b49a6e12ed60
Johan Olofsson <j.olofsson@mail.com>
2017-11-17 14:59:03 +0100

    Added BuildViewModel

---
+++ CaseRod/CaseRod/ViewModels/BuildViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CaseRod.Models;
+
+namespace CaseRod.ViewModels
+{
+    public class BuildViewModel
+    {
+        public List<Blade> Blades { get; set; }
+        public List<Handle> Handles { get; set; }
+        public List<ReelSeat> ReelSeats { get; set; }
+        public Blade ChosenBlade { get; set; }
+        public Handle ChosenHandle { get; set; }
+        public ReelSeat ChosenReelSeat { get; set; }
+        public int Price { get; set; }
+        public int Weight { get; set; }
+    }
+}
