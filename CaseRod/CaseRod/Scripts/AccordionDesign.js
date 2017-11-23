b68f77913e67d2fd9ac5e5c29f942e01cab4b8fd
Magnus Persson <degnus93@hotmail.com>
2017-11-23 13:15:46 +0100

    Changed the AccordionDesign Js

--- CaseRod/CaseRod/Scripts/AccordionDesign.js
+++ CaseRod/CaseRod/Scripts/AccordionDesign.js
@@ -1,19 +1,34 @@
-﻿
-$(function () {
-    "use strict";
-    $('.info').first().show().animate({
-        width: '40%'
-    });
+﻿function accButtons() {
+    var acc = document.getElementsByClassName("accordion");
+    var i;
 
-    $('.item').click(function () {
-        $(this)
-          .next().show().animate({
-              width: '40%'
-          })
-          .siblings(".info").animate({
-              width: 0
-          });
+    for (i = 0; i < acc.length; i++) {
+        acc[i].onclick = function () {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block")
+                panel.style.display = "none";
+            else
+                panel.style.display = "block";
+        }
+    }
+}
 
-    });
+// Js for marking the products
+function productButton() {
+    var prdct = document.getElementsByClassName("product");
+    var i;
 
-});
+    for (i = 0; i < prdct.length; i++) {
+        prdct[i].onclick = function () {
+            this.classList.toggle("checked");
+            if (product.style.backgroundColor === "white")
+                product.style.backgroundColor = "#eee";
+            else
+                product.style.backgroundColor = "white";
+        }
+    }
+}
+function addItem() {
+
+}
