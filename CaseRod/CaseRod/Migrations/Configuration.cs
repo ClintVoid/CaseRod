7e46a4aa4a9fe5bf5ff2d28905a74b409889ce3e
Johan Olofsson <j.olofsson@mail.com>
2017-11-29 10:48:20 +0100

    Added info tab to product page

--- CaseRod/CaseRod/Migrations/Configuration.cs
+++ CaseRod/CaseRod/Migrations/Configuration.cs
@@ -31,22 +31,25 @@ namespace CaseRod.Migrations
             //context.Database.Delete();
 
             context.Blades.AddOrUpdate(
+                b => b.Name,
                 new Blade { Name = "Blade1", Price = 1500, Weight = 670, Image = "Klinga1.png" },
                 new Blade { Name = "Blade2", Price = 1350, Weight = 700, Image = "Klinga2.png" },
                 new Blade { Name = "Blade3", Price = 1600, Weight = 600, Image = "Klinga3.png" }
-                );
+            );
 
             context.Handles.AddOrUpdate(
+                h => h.Name,
                 new Handle { Name = "Handle1", Price = 450, Weight = 67, Image = "Korkhandtag1.png" },
                 new Handle { Name = "Handle2", Price = 550, Weight = 73, Image = "Korkhandtag2.png" },
                 new Handle { Name = "Handle3", Price = 700, Weight = 70, Image = "Korkhandtag3.png" }
-                );
+            );
 
             context.ReelSeats.AddOrUpdate(
+                r => r.Name,
                 new ReelSeat { Name = "ReelSeat1", Price = 1100, Weight = 170, Image = "Rullfaste1.png" },
                 new ReelSeat { Name = "ReelSeat2", Price = 900, Weight = 150, Image = "Rullfaste2.png" },
                 new ReelSeat { Name = "ReelSeat3", Price = 700, Weight = 160, Image = "Rullfaste3.png" }
-                );
+            );
 
 
         }
