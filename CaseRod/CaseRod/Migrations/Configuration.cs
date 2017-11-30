def39806fd249ff70def4be15fc9f84e0635c3c4
Lucas Pettersson Strömsjö <petterssonstromsjo@hotmail.com>
2017-11-30 10:58:30 +0100

    Added price tags and more sample products to db

--- CaseRod/CaseRod/Migrations/Configuration.cs
+++ CaseRod/CaseRod/Migrations/Configuration.cs
@@ -47,14 +47,22 @@ namespace CaseRod.Migrations
                 h => h.Name,
                 new Handle { Name = "Handle1", Price = 450, Weight = 67, Image = "Korkhandtag1.png" },
                 new Handle { Name = "Handle2", Price = 550, Weight = 73, Image = "Korkhandtag2.png" },
-                new Handle { Name = "Handle3", Price = 700, Weight = 70, Image = "Korkhandtag3.png" }
+                new Handle { Name = "Handle3", Price = 700, Weight = 70, Image = "Korkhandtag3.png" },
+                new Handle { Name = "Handle4", Price = 450, Weight = 67, Image = "Handtagtvahands1.png" },
+                new Handle { Name = "Handle5", Price = 550, Weight = 73, Image = "Handtagtvahands2.png" },
+                new Handle { Name = "Handle6", Price = 700, Weight = 70, Image = "Handtagtvahands3.png" }
             );
 
             context.ReelSeats.AddOrUpdate(
                 r => r.Name,
                 new ReelSeat { Name = "ReelSeat1", Price = 1100, Weight = 170, Image = "Rullfaste1.png" },
                 new ReelSeat { Name = "ReelSeat2", Price = 900, Weight = 150, Image = "Rullfaste2.png" },
-                new ReelSeat { Name = "ReelSeat3", Price = 700, Weight = 160, Image = "Rullfaste3.png" }
+                new ReelSeat { Name = "ReelSeat3", Price = 700, Weight = 160, Image = "Rullfaste3.png" },
+                new ReelSeat { Name = "ReelSeat4", Price = 1100, Weight = 170, Image = "RullfasteAlu.png" },
+                new ReelSeat { Name = "ReelSeat5", Price = 900, Weight = 150, Image = "RullfasteDarkWood.png" },
+                new ReelSeat { Name = "ReelSeat6", Price = 700, Weight = 160, Image = "RullfasteGold.png" },
+                new ReelSeat { Name = "ReelSeat7", Price = 1100, Weight = 170, Image = "RullfasteLightWood.png" },
+                new ReelSeat { Name = "ReelSeat9", Price = 700, Weight = 160, Image = "RullfasteRed.png" }
             );
 
 
